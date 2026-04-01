7dedcad8edc5d18ab619f62b9ca4d63144b5e2d4
AASHNA CHAUDHARY <jchaudharyy10@gmail.com>
2026-04-01 21:04:06 +0530

    feat: auth system (signup/login with JWT)

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import authRoutes from "./routes/auth.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Intellmeet API is live and kicking!");
+});
+
+app.use("/api/auth", authRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+    app.listen(5000, () => console.log("Server running on port 5000"));
+  })
+  .catch(err => console.log(err));
